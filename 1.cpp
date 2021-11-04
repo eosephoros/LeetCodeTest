50f78a834cb231d5cc6ee4c5cb156b160f297e7f
eosephoros <super010721@163.com>
2021-11-04 14:35:46 +0800

    init

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+//用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail
+// 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功
+//能。(若队列中没有元素，deleteHead 操作返回 -1 )
+
+class CQueue {
+public:
+    CQueue() {
+
+    }
+    
+    void appendTail(int value) {
+
+    }
+    
+    int deleteHead() {
+
+    }
+};
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue* obj = new CQueue();
+ * obj->appendTail(value);
+ * int param_2 = obj->deleteHead();
+ */
